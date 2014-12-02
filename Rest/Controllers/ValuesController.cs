c04e2506c974ca6ba7c5fadbea20964f08a90406
al223ec <al223ec@student.lnu.se>
2014-12-02 13:45:42 +0100

    Gjort om spa_webservices
    Funkar fortfarande inte med delete, rest verkar måsta göra en del config
    förändringar.

---
+++ Rest/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebService.Models;
+using WebService.Models.Repository;
+
+namespace Rest.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private ReservationRepository repo = new ReservationRepository();
+
+        public Reservation CreateReservation(Reservation reservation)
+        {
+            return repo.Add(reservation);
+        }
+         // GET api/values
+        public IEnumerable<Reservation> Get()
+        {
+            return repo.GetAll();
+        }
+
+        // GET api/values/5
+        public Reservation Get(int id)
+        {
+            return repo.Get(id);
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
